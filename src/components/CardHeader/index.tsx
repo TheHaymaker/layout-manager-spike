61b94158f95767eae3ecf7595b6a4320d9ed6129
Brett Haymaker <brett.haymaker@gmail.com>
2021-10-11 21:41:39 -0500

    feat(ctx-menu): adds context menu with starting options

--- src/components/CardHeader/index.tsx
+++ src/components/CardHeader/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import {MdSettings, MdClose} from 'react-icons/md'
+import { BookMapper } from '..'
+
+const Button = styled.div`
+position: relative;
+display: flex;
+`
 
 const CardHeaderWrapper = styled.div`
 display: flex;
@@ -22,7 +28,8 @@ h6 {
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr;
 
-    button {
+    & button,
+    & div button {
       display: flex;
       align-items: center;
       justify-content: center;
@@ -37,6 +44,49 @@ h6 {
   }
 `
 
+const ContextMenu = styled.div`
+position: absolute;
+top: 0;
+right: 100%;
+width: auto;
+height: auto;
+opacity: 0;
+padding: 8px;
+border-radius: 3px;
+background-color: #fcfcfc;
+box-shadow: 0px 6px 7px -8px black;
+pointer-events: none;
+transition: 0.25s opacity ease-in-out;
+
+& ul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+}
+
+& ul {
+  padding: 0px;
+  margin: 0px;
+  list-style: none;
+  opacity: 0;
+  transition: 0s opacity ease-in-out;
+}
+
+&.visible {
+  display: block;
+  opacity: 1;
+  pointer-events: all;
+  transition: 0.25s opacity ease-in-out;
+}
+
+&.visible ul,
+&.visible ul {
+  opacity: 1;
+  transition: 0.25s opacity ease-in-out;
+}
+`
+
 type CardHeaderProps = {
   title: string;
   viewId: string;
@@ -48,15 +98,63 @@ export const CardHeader: React.FC<CardHeaderProps> = ({
   viewId,
   handleRemoveLayout = null
 }) => {
+
+  const [isVisible, setIsVisible] = useState(false)
+  /**
+   * ---
+   * ## BookMapper State
+   * ---
+   */
+   const [checked, setChecked] = useState([])
+   const [expanded, setExpanded] = useState([])
   return (
     <CardHeaderWrapper>
       <h6>
         {title}
       </h6>
       <div className="btnContainer">
-        <button>
-        <MdSettings />
-        </button>
+        <Button>
+          <button onClick={() => {
+            setIsVisible(prev => !prev)
+          }}>
+            <MdSettings />
+          </button>
+          <ContextMenu className={`${isVisible ? 'visible' : ''}`}>
+            <ul>
+              <li className="listItem" style={{
+                paddingBottom: "5px",
+                borderBottom: "1px solid black"
+              }}>
+                <div style={{
+                  display: "flex",
+                  alignItems: "center"
+                }}>
+                <label style={{
+                  fontSize: "10px"
+                }} htmlFor="hypothetical">Include "Hypothetical" positions?</label>
+                <input name="hypothetical" type="checkbox" />
+                </div>
+              </li>
+              <li className="listItem">
+                <div style={{
+                  display: "block",
+                  width: "max-content"
+                }}>
+                  
+                  <BookMapper
+                    expanded={expanded}
+                    onExpanded={setExpanded}
+                    checked={checked}
+                    onChecked={setChecked}
+                  />
+
+                
+                </div>
+              </li>
+              
+            </ul>
+          </ContextMenu>
+        </Button>
         <button onClick={() => {handleRemoveLayout(viewId)}}>
         <MdClose />
         </button>
